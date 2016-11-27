0204a15d299fcfde66685a742abf6d3f4fd12d0d
Jacob Buysse <Jacob.Buysse@gmail.com>
2016-11-27 13:17:06 -0600

    Handling keyboard in user select.

--- src/components/UserSelect.tsx
+++ src/components/UserSelect.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { IUser } from '../models';
 import * as styles from './UserSelect.scss';
+import * as cx from 'classnames';
 
 interface IUserSelectProps {
 	users: IUser[];
@@ -12,6 +13,7 @@ interface IUserSelectProps {
 interface IUserSelectState {
 	open: boolean;
 	search: string;
+	selection: number;
 }
 
 export default class UserSelect extends React.PureComponent<IUserSelectProps, IUserSelectState> {
@@ -19,7 +21,8 @@ export default class UserSelect extends React.PureComponent<IUserSelectProps, IU
 		super(props);
 		this.state = {
 			open: false,
-			search: ''
+			search: '',
+			selection: 0
 		};
 	}
 
@@ -27,7 +30,8 @@ export default class UserSelect extends React.PureComponent<IUserSelectProps, IU
 		const { users } = this.props;
 		this.setState({
 			open: true,
-			search: ''
+			search: '',
+			selection: 0
 		} as IUserSelectState);
 	};
 
@@ -35,7 +39,8 @@ export default class UserSelect extends React.PureComponent<IUserSelectProps, IU
 		const { value } = e.currentTarget;
 		const { users } = this.props;
 		this.setState({
-			search: value
+			search: value,
+			selection: 0
 		} as IUserSelectState);
 	};
 
@@ -49,12 +54,66 @@ export default class UserSelect extends React.PureComponent<IUserSelectProps, IU
 		};
 	};
 
-	render() {
-		const { placeholder, user, users } = this.props;
+	handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		switch (e.keyCode) {
+			case 13: {
+				this.handleEnter();
+				break;
+			}
+			case 27: {
+				this.handleEscape();
+				break;
+			}
+			case 38: {
+				this.handleUpArrow();
+				break;
+			}
+			case 40: {
+				this.handleDownArrow();
+				break;
+			}
+		}
+	};
+
+	handleEnter = () => {
+		const topUsers = this.getTopUsers();
+		if (topUsers.length > 0) {
+			this.handleSelect(topUsers[this.getSelectedIndex(topUsers.length)])();
+		}
+	};
+
+	handleEscape = () => {
+		this.setState({ open: false } as IUserSelectState);
+	};
+
+	handleUpArrow = () => {
+		const { selection } = this.state;
+		this.setState({ selection: selection - 1 } as IUserSelectState);
+	};
+
+	handleDownArrow = () => {
+		const { selection } = this.state;
+		this.setState({ selection: selection + 1 } as IUserSelectState);
+	};
+
+	getTopUsers = () => {
+		const { users } = this.props;
 		const { open, search } = this.state;
-		const topUsers = users === null ? [] : open ?
+		return users === null ? [] : open ?
 			users.filter(user => user.name.toLowerCase().indexOf(search.toLowerCase()) >= 0).slice(0, 5) :
 			users.slice(0, 5);
+	};
+
+	getSelectedIndex = (count: number) => {
+		const { selection } = this.state;
+		return (((selection % count) + count) % count);
+	};
+
+	render() {
+		const { placeholder, user } = this.props;
+		const { open, search } = this.state;
+		const topUsers = this.getTopUsers();
+		const selectedIndex = this.getSelectedIndex(topUsers.length);
 		const inputValue = open ? search : user ? user.name : '';
 
 		return(
@@ -73,13 +132,19 @@ export default class UserSelect extends React.PureComponent<IUserSelectProps, IU
 							placeholder={placeholder}
 							value={inputValue}
 							autoFocus
-							onChange={this.handleSearch} />
+							onChange={this.handleSearch}
+							onKeyDown={this.handleKeyDown} />
 					</span>
 				}
 				{open &&
 					<div className={styles.users}>
 						{topUsers.map((user, i) => (
-							<div key={i} className={styles.user} onClick={this.handleSelect(user)}>{user.name}</div>
+							<div
+								key={i}
+								className={cx(styles.user, { [styles.selected]: i === selectedIndex })}
+								onClick={this.handleSelect(user)}>
+								{user.name}
+							</div>
 						))}
 					</div>
 				}
