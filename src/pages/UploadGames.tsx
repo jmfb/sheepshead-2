80af6c84520183b319cbc9ebc724cdf087124645
Greg Smith <smr.kyu@gmail.com>
2016-11-30 12:49:06 -0600

    CSS updates

--- src/pages/UploadGames.tsx
+++ src/pages/UploadGames.tsx
@@ -30,38 +30,30 @@ export default class UploadGames extends React.PureComponent<IUploadGamesProps, 
 		const { games, pendingFileCount, submitting, onClickSubmit } = this.props;
 		return(
 			<div className={styles.root}>
-				<div className={styles.top}>
-					<div className={styles.topInner}>
-						<h1 className={styles.title}>Upload Games</h1>
-						<input
-							type='file'
-							multiple
-							accept='*.json'
-							onChange={this.handleChooseFile} />
-						<div className={styles.progress}>
-							{games.length > 0 && pendingFileCount === 0 && !submitting &&
-								<Button type='primary' display='Submit' onClick={onClickSubmit} />
-							}
-							{submitting &&
-								<Banner type='message' display={`Uploading ${games.length} games...`} />
-							}
-							{pendingFileCount > 0 &&
-								<Banner type='message' display={`Parsing ${pendingFileCount} files...`} />
-							}
-							{games.length === 0 && pendingFileCount === 0 && !submitting &&
-								<Banner type='message' display='Select a .json file to upload.' />
-							}
-						</div>
-					</div>
+				<h1>Upload Games</h1>
+				<input
+					type='file'
+					multiple
+					accept='*.json'
+					onChange={this.handleChooseFile} />
+				<div className={styles.progress}>
+					{games.length > 0 && pendingFileCount === 0 && !submitting &&
+						<Button type='primary' display='Submit' onClick={onClickSubmit} />
+					}
+					{submitting &&
+						<Banner type='message' display={`Uploading ${games.length} games...`} />
+					}
+					{pendingFileCount > 0 &&
+						<Banner type='message' display={`Parsing ${pendingFileCount} files...`} />
+					}
+					{games.length === 0 && pendingFileCount === 0 && !submitting &&
+						<Banner type='message' display='Select a .json file to upload.' />
+					}
 				</div>
-				<div className={styles.bottom}>
-					<div className={styles.bottomInner}>
-						<div className={styles.count}>{games.length} Games</div>
-						{games.map((game, i) => (
-							<Game key={i} game={game} />
-						))}
-					</div>
-				</div>
+				<div className={styles.count}>{games.length} Games</div>
+				{games.map((game, i) => (
+					<Game key={i} game={game} />
+				))}
 			</div>
 		);
 	}
